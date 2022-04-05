371e98d50e5261494c0710326cde6c1d8b0f1a46
kongdesheng <kongdesheng@focusmedia.cn>
2022-04-05 19:33:41 +0800

    chore: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -16,8 +16,14 @@ git init
 git add -A
 git commit -m 'deploy'
 
+git remote add origin https://github.com/keeperdog/keeperdog.github.io.git
+
+git push https://github.com/keeperdog/keeperdog.github.io.git main
+
 # 如果你想要部署到 https://USERNAME.github.io
-git push -f https://github.com/keeperdog/keeperdog.github.io.git main
+# git push -f https://github.com/keeperdog/keeperdog.github.io.git main
+# 另一种方式亦可
+# git push --set-upstream origin main 
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
